da381569b67bc6db6d59b465b759cf66faaf8212
hbrittany <hogg0019@algonquinlive.com>
2014-09-30 10:47:03 -0400

    Added Files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var salesDetails = $(".sales__details");
+var sales = $(".sales");
+
+var buttonClickHandler = function () {
+	var isActive = salesDetails.attr("data-state");
+
+	if (isActive == "active") {
+		salesDetails.attr("data-state", "inactive");
+	} else {
+		salesDetails.attr("data-state", "active");	
+	}
+
+};
+
+
+sales.on("click", buttonClickHandler);
+
